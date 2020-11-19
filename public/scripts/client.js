65c046dd0309c80c00cebea3e23c65a6cd8dd466
mohdtorabi <mitq8.mt@gmail.com>
2020-11-19 23:48:46 +0000

    removed extra lines and refactored some codes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         </header>
         <p class = "tweet-content">${escape(tweet.content.text)}</p>
         <footer> 
-          <span>${getDate(tweet.created_at)} days ago</span>
+          <span>${getDate(tweet.created_at)} ago</span>
           <div>
             <button class = "symbols" type="button">&#9873</button>
             <button class = "symbols" type="button">&#9850</button>
@@ -35,11 +35,11 @@ $(document).ready(function() {
   };
   renderTweets(data);
 
+  
   const submitButton = () => {
     $(".tweet-form").submit(function(event) {
       event.preventDefault();
       const text = $("#tweet-text").val();
-      console.log(text);
       if (text.length === 0) {
         $(".error-container").text("Error: Cannot post an empty tweet");
         $(".error-container").css("border-color", "red");
@@ -64,6 +64,7 @@ $(document).ready(function() {
   };
   submitButton();
 
+
   const loadTweets = () => {
     $.ajax("/tweets/")
       .then((data) => {
@@ -71,6 +72,7 @@ $(document).ready(function() {
       });
   };
   loadTweets();
+
 
   const escape = string => {
     let div = document.createElement("div");
@@ -80,24 +82,27 @@ $(document).ready(function() {
 
 });
 
-
-const getDate = (milliseconds) => {
+//get date function(instruction from stackoverflow)
+const getDate = milliseconds => {
   const datePosted = new Date(milliseconds);
   const dateNow = new Date().getTime();
   const time = Math.abs(dateNow - datePosted);
-  let diff;
-
+  let sum;
   if (time < 1000 * 60) {
-    diff = Math.floor(time / (1000));
+    sum = Math.floor(time / (1000));
+    unit = "S";
   } else if (time < 1000 * 60 * 60) {
-    diff = Math.floor(time / (1000 * 60));
-  } else if (time < 1000 * 60 * 60 * 24) {
-    diff = Math.floor(time / (1000 * 60 * 60));
+    sum = Math.floor(time / (1000 * 60));
+    unit = "m";
+  } else if (time < 1000 * 60 * 60 * 60) {
+    sum = Math.floor(time / (1000 * 60 * 60));
+    unit = "h";
   } else {
-    diff = Math.floor(time / (1000 * 60 * 60 * 60));
+    sum = Math.floor(time / (1000 * 60 * 60 * 60));
+    unit = "d";
   }
   
-  return `${diff}`;
+  return `${sum} ${unit}`;
 };
 
 
